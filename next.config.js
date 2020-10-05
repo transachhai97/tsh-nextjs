e922a88a61154cf69714c2d0237ed56167ac8855
HaiTS <transachhai97@gmail.com>
2020-10-05 09:21:04 +0700

    sass

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-module.exports = {
+const withPlugins = require('next-compose-plugins');
+const sass = require('@zeit/next-sass');
+
+const nextConfig = {
     // Target must be serverless
     target: 'serverless'
 };
+
+module.exports = withPlugins([
+    [
+        sass,
+        {
+            cssModules: true
+        }
+    ],
+], nextConfig);
